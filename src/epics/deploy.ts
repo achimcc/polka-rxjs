aad6f7f8d30306b880d3fe80eb858e33ff2732dc
achimcc <achim.schneider@posteo.de>
2021-04-30 08:26:17 +0200

    progress on contract deployment

--- src/epics/deploy.ts
+++ src/epics/deploy.ts
@@ -4,15 +4,21 @@ import { map, mergeMap } from "rxjs/operators";
 import { ApiRx, Keyring } from "@polkadot/api";
 import { CodeRx, Abi } from "@polkadot/api-contract";
 import type { AnyJson } from "@polkadot/types/types";
+import { RawParams } from "./utils/types";
+import { createValue, computeValues } from "./utils/values";
 
 export const deploy = (action$: ActionsObservable<Action>, state$: any) =>
   action$.ofType("Deploy").pipe(
     mergeMap((action) => {
-      const abi = new Abi((state$ as any).value.contract.abi as AnyJson);
-      console.log("abi: ", abi);
       const api = (state$ as any).value.contract.api as ApiRx;
+      const abi = new Abi(
+        (state$ as any).value.contract.abi as AnyJson,
+        api.registry.getChainProperties()
+      );
+      const params = abi.constructors[0].args;
+      const registry = api.registry;
+      const values = computeValues(params, registry);
       const wasm = (state$ as any).value.contract.wasm as Uint8Array;
-      console.log("wasm: ", wasm);
       const code = new CodeRx(api, abi, wasm);
       const contract = code.createContract(0, { gasLimit: 0, value: 0 }, []);
       const keyring = new Keyring({ type: "sr25519" });
